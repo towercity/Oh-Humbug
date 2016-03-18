cd71ae890f47447c52b07714c28d85d7a8a7d797
Matthew Nerger (l) <mnerger@lsu.edu>
2016-03-18 08:03:57 -0500

    Begin Player() prototype

--- js/app.js
+++ js/app.js
@@ -22,9 +22,21 @@ Enemy.prototype.render = function() {
 };
 
 // Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-horn-girl.png';
+};
 
+Player.prototype.update = function(dt) {
+  //null
+};
+
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function() {
+  //null
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
